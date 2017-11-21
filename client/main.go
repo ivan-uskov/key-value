018944c4da0ffa7b766b584aa2c594963dd30b71
Ivan Uskov <ivan.uskov@cpslabs.net>
2017-11-21 09:00:47 +0300

    Add websocket client

--- client/main.go
+++ client/main.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"flag"
-	"log"
 	"key-value/lib/ws"
-	"time"
+	"fmt"
+	"os"
+	"log"
+	"bufio"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
 	flag.Parse()
-	log.SetFlags(0)
 
 	client := ws.NewClient(*addr, `ws`)
 
-	for {
-		select {
-			case <-time.After(5 * time.Second):
-				mc, err := client.Send(`{"Action":"LIST"}`)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				log.Println(<-mc)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		res, err := client.SendSync(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
+		fmt.Println(res)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
